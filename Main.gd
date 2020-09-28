5f23e2c198e3e02c782782457e597bd3f212f3fe
CroweIT <croweit@gmail.com>
2020-09-28 16:44:45 +1000

    Added assets, blocked out space for upper and lower GUI, restructured tree to fit GUI

--- Main.gd
+++ Main.gd
@@ -6,9 +6,9 @@ var launches = 0
 
 func change_money(delta_dollar):
 	money += delta_dollar
-	$GUI/Cols/Stats/Rows/Money.text = "$" + str(money)
+	$GUI/Box/Cols/Stats/Rows/Money.text = "$" + str(money)
 
 
 func increment_launches():
 	launches += 1
-	$GUI/Cols/Stats/Rows/Launches/Counter.text = str(launches)
+	$GUI/Box/Cols/Stats/Rows/Launches/Counter.text = str(launches)
